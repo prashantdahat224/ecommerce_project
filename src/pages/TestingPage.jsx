b1bc772d2e23e7c8fe268f8f66442f14de45e06e
prashant dahat <prashantdahat224@gmail.com>
2026-03-30 13:59:03 -0700

    latest changes 014

--- src/pages/TestingPage.jsx
+++ src/pages/TestingPage.jsx
@@ -1,1 +1,29 @@
-//just a testing page 
+import { useEffect, useState } from "react";
+  
+function TestingPage() {
+
+ 
+
+//console.log("useParams",useParams())
+  
+  
+  //  useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     setLoading(false);
+  //   }, 1000);  
+
+  //   return () => clearTimeout(timer);
+  // }, []);
+
+ 
+
+  return (
+    <div className="bg-white min-h-screen pb-20 lg:mx-60">
+       
+       <p>Code testing page</p>
+     
+    </div>
+  );
+}
+
+export default TestingPage;
